071be9a91368853867c2a64064374e4cd148b01c
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2024-06-12 16:39:51 +0200

    feat: filter

--- src/graphql/runs/resolvers/query/runs.ts
+++ src/graphql/runs/resolvers/query/runs.ts
@@ -3,11 +3,34 @@ import {
   RoutePlanningRepository,
 } from '../../../../repositories';
 
-export const runs = async () => {
+type Options = {
+  filter?: {
+    type?: string;
+  };
+};
+
+export const runs = async (_parent: unknown, options?: Options) => {
+  switch (options?.filter?.type) {
+    case 'EmployeeScheduling':
+      return EmployeeSchedulingRepository.getAll();
+    case 'FieldServiceRouting':
+      return RoutePlanningRepository.getAll();
+  }
+
   const runs = [
     ...EmployeeSchedulingRepository.getAll(),
     ...RoutePlanningRepository.getAll(),
-  ];
+  ].sort((a, b) => {
+    if (new Date(a.submitTime) > new Date(b.submitTime)) {
+      return -1;
+    }
+
+    if (new Date(a.submitTime) < new Date(b.submitTime)) {
+      return 1;
+    }
+
+    return 0;
+  });
 
   return runs;
 };
